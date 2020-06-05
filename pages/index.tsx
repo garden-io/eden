c62b03803ed984fab63c74b3780e350b577f36be
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-05 18:50:33 +0200

    feat: better color picker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,31 @@
 import React from "react"
-import { Box, GlobalStyles, Text, Title, Space, Grid, colors, Code, GardenLogo } from "../src"
+import { Box, GlobalStyles, Text, Space, Grid, colors, Code, Section, Color } from "../src"
 
 const Index = () => {
   return (
     <>
       <GlobalStyles />
-      <Title size="large">Eden</Title>
-      <Space height="larger" />
-      <Space />
-      <Grid gap="large">
-        <Title color="gray">Colors</Title>
-        <Grid columns={5}>
-          {Object.entries(colors).map(([key, value]) => (
-            <Grid key={key} gap="small">
-              <div
-                style={{
-                  background: colors[key],
-                }}
-              >
-                <br />
-                <br />
-                <br />
-              </div>
-              <Code color="gray">{key}</Code>
-              <Code color="gray">{value}</Code>
-              <Space />
-            </Grid>
-          ))}
-        </Grid>
+      <Space height="largest" />
+      <Section>
         <Space height="larger" />
-        <Title color="gray">Logo</Title>
-        <Grid columns="2fr 5fr" gap="larger">
-          <Code color="gray">{`<GardenLogo />`}</Code>
-          <GardenLogo />
-          <Code color="gray">{`<Box background="grayDarker"><GardenLogo color="light" /></Box>`}</Code>
-          <Box background="grayDarker">
-            <GardenLogo color="light" />
-          </Box>
+        <Text size="large">Eden documentation</Text>
+        <Space />
+        <Grid gap="large">
+          <Text>Colors</Text>
+          <Grid columns={6} gap="none">
+            {Object.entries(colors).map(([key, value]) => (
+              <Box key={key} background={key as Color} height="160px" align="flex-end">
+                <Code size="small" color="black">
+                  {key}
+                </Code>
+                <Code size="small" color="black">
+                  {value}
+                </Code>
+              </Box>
+            ))}
+          </Grid>
         </Grid>
-      </Grid>
+      </Section>
     </>
   )
 }
