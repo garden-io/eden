4ee6a0920a037eb893fc22da249fb2b61e242b51
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-05 20:26:31 +0200

    feat: docs with gatsby (#18)

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { GlobalStyles, Title, Section, Space } from ".."
+
+const Index = () => (
+  <>
+    <GlobalStyles />
+    <Section width="small">
+      <Space height="largest" />
+      <Title>Hello World</Title>
+    </Section>
+  </>
+)
+
+export default Index
