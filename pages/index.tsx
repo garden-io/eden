185d6e1b6f3b49b6ac321445331957c5fc84d2a4
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-05 18:12:53 +0200

    feat: adding nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Box, GlobalStyles, Text } from "../src"
+
+const Index = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Box background="greenDark">
+        <Text>Hello</Text>
+      </Box>
+    </>
+  )
+}
+
+export default Index
