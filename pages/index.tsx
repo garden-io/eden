2a9351df041b05b2eb15a08a74b5f519c268c705
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-05 18:32:03 +0200

    feat: add public dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,44 @@
 import React from "react"
-import { Box, GlobalStyles, Text } from "../src"
+import { Box, GlobalStyles, Text, Title, Space, Grid, colors, Code, GardenLogo } from "../src"
 
 const Index = () => {
   return (
     <>
       <GlobalStyles />
-      <Box background="greenDark">
-        <Text>Hello</Text>
-      </Box>
+      <Title size="large">Eden</Title>
+      <Space height="larger" />
+      <Space />
+      <Grid gap="large">
+        <Title color="gray">Colors</Title>
+        <Grid columns={5}>
+          {Object.entries(colors).map(([key, value]) => (
+            <Grid key={key} gap="small">
+              <div
+                style={{
+                  background: colors[key],
+                }}
+              >
+                <br />
+                <br />
+                <br />
+              </div>
+              <Code color="gray">{key}</Code>
+              <Code color="gray">{value}</Code>
+              <Space />
+            </Grid>
+          ))}
+        </Grid>
+        <Space height="larger" />
+        <Title color="gray">Logo</Title>
+        <Grid columns="2fr 5fr" gap="larger">
+          <Code color="gray">{`<GardenLogo />`}</Code>
+          <GardenLogo />
+          <Code color="gray">{`<Box background="grayDarker"><GardenLogo color="light" /></Box>`}</Code>
+          <Box background="grayDarker">
+            <GardenLogo color="light" />
+          </Box>
+        </Grid>
+      </Grid>
     </>
   )
 }
