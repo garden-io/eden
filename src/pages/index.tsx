e47d1edad058add61f004dfbd44235a83c20c206
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-08 10:19:16 +0200

    chore: remove debug info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 
-import { GlobalStyles, Title, Text, Section, Space, breakpoints, Pre } from ".."
+import { GlobalStyles, Title, Text, Section, Space } from ".."
 
 const Index = () => (
   <>
     <GlobalStyles />
     <Section width="small">
       <Space height="largest" />
-      <Pre>{JSON.stringify(breakpoints)}</Pre>
       <Title fontWeight="normal">
         Hello <b>World</b>
       </Title>
