6e414ec4ebc295f3d246c64bddd878fdba5d3d49
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-09 13:31:06 +0200

    fix: gradient values

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -8,11 +8,9 @@ import React, { FC } from "react"
 
 const gradients = {
   green: `linear-gradient(163deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)`,
-  greenReverse: `linear-gradient(${
-    163 + 180
-  }deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)`,
+  greenReverse: `linear-gradient(343deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)`,
   blue: `linear-gradient(163deg, hsl(197, 52%, 94%) 3%, rgba(255, 255, 255, 0) 98%), rgba(255, 255, 255, 0)`,
-  blueReverse: `linear-gradient(${154 + 180}deg, hsl(196.5,86%,94.3%) -38.89%, rgba(255, 255, 255, 0) 79.45%)`,
+  blueReverse: `linear-gradient(334deg, hsl(196,86%,94%) -39%, rgba(255, 255, 255, 0) 79%)`,
 }
 
 type Props = {
