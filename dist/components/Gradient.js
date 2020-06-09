e0099cdb7cc0ff0c8155a1908d867def621b6392
kristjanjansen <kristjanjansen@users.noreply.github.com>
2020-06-09 11:40:14 +0000

    Build the library

--- dist/components/Gradient.js
+++ dist/components/Gradient.js
@@ -12,8 +12,9 @@ var react_1 = __importDefault(require("react"));
 var gradients = {
     green: "linear-gradient(163deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)",
     greenReverse: "linear-gradient(343deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)",
+    //greenReverse: `linear-gradient(343deg, hsl(174, 52%, 94%) 21%, rgba(255, 255, 255, 0) 88%), rgba(255, 255, 255, 0)`,
     blue: "linear-gradient(163deg, hsl(197, 52%, 94%) 3%, rgba(255, 255, 255, 0) 98%), rgba(255, 255, 255, 0)",
-    blueReverse: "linear-gradient(334deg, hsl(196,86%,94%) -39%, rgba(255, 255, 255, 0) 79%)",
+    blueReverse: "linear-gradient(343deg, hsl(197, 52%, 94%) 3%, rgba(255, 255, 255, 0) 98%), rgba(255, 255, 255, 0)",
 };
 exports.Gradient = function (_a) {
     var _b = _a.color, color = _b === void 0 ? "green" : _b, children = _a.children;
