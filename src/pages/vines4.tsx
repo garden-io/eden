cf48ebbf1b1462febc4eb4473a958c1849fde65b
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-07-10 16:18:46 +0300

    fix: cleanup gradientmasks

--- src/pages/vines4.tsx
+++ src/pages/vines4.tsx
@@ -14,13 +14,9 @@ const Svg = ({ children }) => (
 )
 const Vine = () => {
   const [r, setR] = useState(270)
-  //const path =
-  "M20 20H90C96.5761 20.077 109.783 24.1847 110 40C110.217 55.8153 110.091 126.59 110 160C109.961 166.589 113.906 179.813 130 180C146.094 180.187 170.039 180.078 180 180"
   const path =
     "M20 20H80C86.5 19.9 99.6 23.76 100 40C100.4 56.24 100.167 86.7667 100 100C99.7667 106.638 103.44 119.932 120 120C136.56 120.068 146.9 120.028 150 120C156.641 119.733 169.938 123.36 170 140C170.062 156.64 170.026 173.6 170 180"
-  // const outlinepath =
-  //   "M89.9681 25H20V15H90.0293L90.0585 15.0003C94.1523 15.0483 100.165 16.3081 105.322 20.052C110.716 23.9679 114.869 30.4064 115 39.9313C115.218 55.7997 115.09 126.632 115 160.014L115 160.022L115 160.03C114.985 162.505 115.76 166.36 118.006 169.477C120.074 172.347 123.602 174.925 130.058 175C146.101 175.187 170.006 175.078 179.961 175L180.039 185C170.072 185.078 146.087 185.187 129.942 185C120.304 184.888 113.812 180.761 109.894 175.324C106.157 170.14 104.978 164.094 105 159.981C105.091 126.539 105.217 55.83 105 40.0687C104.914 33.7784 102.355 30.2554 99.4471 28.1443C96.3136 25.8694 92.4515 25.0347 89.9681 25Z"
-  // let [left, top, right, bottom] = getBounds(outlinepath)
+
   var segments = normalize(abs(parse(path)))
     .flat()
     .join(" ")
@@ -33,13 +29,21 @@ const Vine = () => {
   const width = right - left + 2 * rr
   const height = bottom - top + 2 * rr
 
-  const leftGradient = {
+  const topGradient = {
     opacities: [0, 0, 1, 1, 1, 1],
+    rotate: 90,
+  }
+  const rightGradient = {
+    opacities: [1, 1, 1, 1, 0, 0],
     rotate: 0,
   }
   const bottomGradient = {
     opacities: [1, 1, 1, 1, 0, 0],
     rotate: 90,
+  }
+  const leftGradient = {
+    opacities: [0, 0, 1, 1, 1, 1],
+    rotate: 0,
   }
 
   const GradientMask = ({ opacities, rotate, x, y, width, height, id }) => {
@@ -63,32 +67,13 @@ const Vine = () => {
       <input max="360" type="range" value={r} onChange={(e) => setR(parseFloat(e.target.value))} />
       <Svg>
         <defs>
-          <GradientMask {...leftGradient} x={x} y={y} width={width} height={height} id="leftGradient" />
-          {/* <linearGradient id="gradient">
-            <stop offset="0" stopColor="white" stopOpacity="0" />
-            <stop offset="0.03" stopColor="white" stopOpacity="0" />
-            <stop offset="0.1" stopColor="white" stopOpacity="1" />
-            <stop offset="0.9" stopColor="white" stopOpacity="1" />
-            <stop offset="0.97" stopColor="white" stopOpacity="1" />
-            <stop offset="1" stopColor="white" stopOpacity="1" />
-          </linearGradient>
-          <mask id="gradient-mask">
-            <rect x={x} y={y} width={width} height={height} fill="url(#gradient)" />
-          </mask>
-          <linearGradient id="gradient2" gradientTransform="rotate(90)">
-            <stop offset="0" stopColor="white" stopOpacity="1" />
-            <stop offset="0.03" stopColor="white" stopOpacity="1" />
-            <stop offset="0.1" stopColor="white" stopOpacity="1" />
-            <stop offset="0.9" stopColor="white" stopOpacity="1" />
-            <stop offset="0.97" stopColor="white" stopOpacity="0" />
-            <stop offset="1" stopColor="white" stopOpacity="0" />
-          </linearGradient>
-          <mask id="gradient-mask2">
-            <rect x={x} y={y} width={width} height={height} fill="url(#gradient2)" />
-          </mask> */}
+          <GradientMask {...topGradient} x={x} y={y} width={width} height={height} id="topGradient" />
+          <GradientMask {...bottomGradient} x={x} y={y} width={width} height={height} id="bottomGradient" />
         </defs>
-        <g mask="url(#leftGradient)">
-          <path d={outlinepath} />
+        <g mask="url(#topGradient)">
+          <g mask="url(#bottomGradient)">
+            <path d={outlinepath} />
+          </g>
         </g>
         <path d={path} stroke="blue" fill="none" />
         <rect x={x} y={y} width={width} height={height} fill="none" stroke="red" />
