6511433e155a48c26f935cfe5855936e2aa30ed6
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-07-10 17:36:01 +0300

    chore: cleanup

--- src/pages/vines4.tsx
+++ src/pages/vines4.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react"
-import { colors } from ".."
+import { colors, Box } from ".."
 import getBounds from "svg-path-bounds"
 import pathOutline from "svg-path-outline"
+import { Gradient } from "../components/Gradient"
 
 var parse = require("parse-svg-path")
 var abs = require("abs-svg-path")
@@ -103,7 +104,7 @@ const AnimateShine = ({ path, outlinepath, fill, id }) => (
       </filter>
     </defs>
     <g clipPath={"url(#" + id + "-clip)"}>
-      <rect x="-200" y="-100" width="200" height="200" fill={fill} filter="url(#blur)">
+      <rect x="-400" y="-200" width="400" height="400" fill={fill} filter="url(#blur)">
         <animateMotion id="one" dur="3s" rotate="auto" fill="freeze" calcMode="linear" begin="0s">
           <mpath xlinkHref={"#" + id + "-path"} />
         </animateMotion>
@@ -123,7 +124,7 @@ const Vine = () => {
   const path =
     "M20 20H80C86.5 19.9 99.6 23.76 100 40C100.4 56.24 100.167 86.7667 100 100C99.7667 106.638 103.44 119.932 120 120C136.56 120.068 146.9 120.028 150 120C156.641 119.733 169.938 123.36 170 140C170.062 156.64 170.026 173.6 170 180"
   const path2 =
-    "M100 40V70C100.072 76.7744 96.1723 90.2585 80 90C63.8277 89.7415 53.2615 89.8923 50 90C43.3692 90.2128 30.0862 94.5108 30 110C29.9138 125.489 29.9641 136.454 30 140C29.7974 146.744 33.5138 160.185 50 160C66.4862 159.815 70.2026 173.256 70 180"
+    "M100 40V70C100.072 76.7744 96.1723 90.2585 80 90C63.8277 89.7415 53.2615 89.8923 50 90C43.3692 90.2128 30.0862 94.5108 30 110C29.9138 125.489 29.9641 176.838 30 180.385"
   const outlinepath = getPathOutline(path)
   const outlinepath2 = getPathOutline(path2)
 
@@ -131,8 +132,8 @@ const Vine = () => {
     <>
       <Svg>
         <FadeEdges path={path} r={6} top bottom>
-          <path d={outlinepath2} fill={colors.greenDark} />
-          <AnimateShine id="two" path={path2} outlinepath={outlinepath2} fill={colors.greenLight} />
+          <path d={outlinepath2} fill={colors.greenLight} />
+          <AnimateShine id="two" path={path2} outlinepath={outlinepath2} fill={colors.greenDark} />
         </FadeEdges>
         <FadeEdges path={path} r={6} left bottom>
           <path d={outlinepath} fill={colors.greenDark} />
@@ -145,8 +146,11 @@ const Vine = () => {
 
 const Page = () => (
   <>
-    <style>{`body { background: ${colors.blueDark}; }`}</style>
-    <Vine />
+    <style>{`body { margin: 0; }`}</style>
+    <Gradient color="blueReverse">
+      <Vine />
+      <Box height="50vh" />
+    </Gradient>
   </>
 )
 
